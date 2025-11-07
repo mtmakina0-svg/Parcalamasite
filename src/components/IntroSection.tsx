60da49c53b6077fce72ba634c9be04dc8768cff0
fkestek <mtmakina0@gmail.com>
2025-11-07 16:30:49 +0300

    Update IntroSection.tsx

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'motion/react';
+import { motion } from "framer-motion";
 import { useLanguage } from './LanguageContext';
 import { Leaf, Recycle, Shield, Zap } from 'lucide-react';
 
