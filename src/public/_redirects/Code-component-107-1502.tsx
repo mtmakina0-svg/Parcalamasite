f1ad38ea316c771d5acbd9b6d2778b10ae8e0c62
mtmakina0-svg <mtmakina0@gmail.com>
2025-11-07 18:09:49 +0300

    Delete src/public/_redirects directory

--- src/public/_redirects/Code-component-107-1502.tsx
+++
@@ -1,5 +0,0 @@
-# Netlify Redirects for SPA (Single Page Application)
-# This file ensures that all routes are handled by the React app
-
-# Redirect all routes to index.html (SPA fallback)
-/*    /index.html   200
--- src/public/_redirects/Code-component-256-1137.tsx
+++
@@ -1,9 +0,0 @@
-# Netlify Redirects for MT Makina SPA
-# IMPORTANT: This is a TEXT file, not a directory!
-
-# Sitemap and robots - serve as static files
-/sitemap.xml    /sitemap.xml    200!
-/robots.txt     /robots.txt     200!
-
-# SPA fallback - all other routes go to index.html
-/*              /index.html     200
